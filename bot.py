98b801244f0dbce3928aa8f7ad9d153cd2a762f4
kasparspurins1987-cyber <kaspars.purins1987@gmail.com>
2026-06-12 19:24:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
 import telebot
 import requests
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-# Pārliecinies, ka tev šeit ir pareizais bota token
-TOKEN = 'TEV_JABUT_TAVAM_BOT_TOKEN'
+# Tavs tokens
+TOKEN = '8871535091:AAEmR6qWY-zcI5iLmli_5dJoIPuVugRt_kM'
 bot = telebot.TeleBot(TOKEN)
 
-# HTTP serveris, lai Render.com nedomātu, ka bots ir "miris"
+# HTTP serveris, lai Render neizslēgtu botu dēļ neaktivitātes
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        # Šeit izmantojam .encode('utf-8'), lai nebūtu kļūdu ar latviešu burtiem
         self.wfile.write("Bots ir aktivs".encode('utf-8'))
 
 def run_server():
@@ -22,9 +21,15 @@ def run_server():
 # Palaižam serveri paralēli botam
 threading.Thread(target=run_server, daemon=True).start()
 
-# Tālāk nāk tavs pārējais bota kods...
-# (Pārliecinies, ka arī pārējās rindiņās, kur raksti tekstus, 
-# izmanto normālus burtus vai .encode('utf-8'), ja tas ir baitu formāts)
+# Šeit sākas tavs bota loģikas kods
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Sveiks! Bots ir veiksmīgi palaists uz Render.com")
 
+# Šeit vari likt pārējo savu kodu (kripto cenas utt.)
+# Pārliecinies, ka visos print vai tekstos izmanto .encode('utf-8') vai izvairies no garumzīmēm
+
+print("Bots darbojas!")
 bot.polling(none_stop=True)
+
 
